8c29d573cc2866b81b707e57ed079c21b5a9007e
zzfima <zzfima@gmail.com>
2024-09-27 17:06:03 +0300

    start

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+            Console.Write("lol");
+            Console.WriteLine("Helloo");
+            Console.Write("papa");
+
+
+            Console.WriteLine("plese insert number");
+            string s = Console.ReadLine();
+            int num = int.Parse(s);
+
+
+            Console.WriteLine(s);
+        }
+
+        private static int CalcAdd(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void CalcAddPrint(int a, int b)
+        {
+            Console.Write(a + b);
+        }
+    }
+}
